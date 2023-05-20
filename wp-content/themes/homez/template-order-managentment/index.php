e480abe06e7c71a48d53dd934b976e4cc92b1346
Vu Cong Thuong <congthuong147@gmail.com>
2023-05-20 18:10:54 +0700

    Update code

--- wp-content/themes/homez/template-order-managentment/index.php
+++ wp-content/themes/homez/template-order-managentment/index.php
@@ -4,12 +4,12 @@
   $i=1;
   $total_amount = 0;
   $total_return_agent = 0;
+  $total_debt=0;
 
   foreach ($all_orders as $order)
   {
     $data_order = $order->get_base_data();
-    $meta_data = $order->get_meta('custom_order_field');
-
+    $date_order = $data_order['date_created']->date("d-m-Y");
     $items = $order->get_items();
     $product_name = '';
     $agent ='';
@@ -19,10 +19,42 @@
       $product_id =$item->get_data()['product_id'];
       $agent = wc_get_product($product_id)->short_description;
   }
+    // Only admin and agent are allowed
     if (current_user_can('wp_realestate_agent') && $agent != $current_user->user_login)
     {
       continue;
     }
+
+    // Process search
+    if ($_POST['name-product'] && !str_contains($product_name, $_POST['name-product']))
+      {
+        continue;
+      }
+
+    if ($_POST['status-order'] && !str_contains($_POST['status-order'], $data_order['status']))
+      {
+        continue;
+      }
+
+      if ($_POST['start-date'] && $_POST['start-date'] > $date_order)
+      {
+        continue;
+      }
+
+      if ($_POST['end-date'] && $_POST['end-date'] < $date_order)
+      {
+        continue;
+      }
+
+
+      // Process action to calculate debt
+    $meta_data = $order->get_meta('custom_order_field');
+    if (!$meta_data)
+      {
+        $total_debt += $price_agent;
+      }
+
+
     $price_agent = round($data_order['total'] * 0.99);
     $price_host = round($data_order['total'] * 0.1);
     $total_amount += $data_order['total'];
@@ -33,11 +65,11 @@
     $data .= '<td><a href="'.admin_url( 'post.php?post=' . $data_order['id'] . '&action=edit') .'">'.$data_order['id'].'</a></td>';
     $data .= '<td>'.$agent.'</td>';
     $data .= '<td>'.$product_name.'</td>';
-    $data .= '<td class="text-center">'.$data_order['date_created']->date("Y-m-d H:i:s").'</td>';
+    $data .= '<td class="text-center">'.$data_order['date_created']->date("d-m-Y H:i:s").'</td>';
     $data .= '<td>'.wc_price($data_order['total']).'</td>';
     $data .= '<td>'.wc_price($price_agent).'</td>';
     $data .= '<td>'.wc_price($price_host).'</td>';
-    $data .= '<td>'.$data_order['status'].'</td>';
+    $data .= '<td>'.wc_get_order_statuses()['wc-'.$data_order['status']].'</td>';
     $data .= '<td class="text-center">'.$meta_data.'</td>';
     $data .= '</tr>';
   }
@@ -55,6 +87,18 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css">
+      <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+  <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+  <script>
+  $( function() {
+    $( "#start-date, #end-date" ).datepicker({
+      format: 'dd-mm-yyyy',
+      constrainInput: false
+    });
+  });
+  </script>
     <title>Quản lý Order</title>
 </head>
 <body>
@@ -141,7 +185,7 @@
                           <div class="col mr-2">
                               <div class="text-xs font-weight-bold text-warning text-uppercase mb-1 fs-4">
                                   Debt</div>
-                              <div class="h5 mb-0 font-weight-bold text-gray-800">500</div>
+                              <div class="h5 mb-0 font-weight-bold text-gray-800"><?=wc_price($total_debt)?></div>
                           </div>
                           <div class="col-auto">
                               <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
@@ -156,10 +200,11 @@
             <div class="col-12">
               <div class="card border-left-success shadow h-100 py-2">
                 <div class="card-body">
+                  <form action="?search" method="post">
                     <div class="row no-gutters align-items-center">
                         <div class="col-2">
                           <div class="input-group date" id="datetimepicker1" data-target-input="nearest">
-                            <input id="dt1" type="text" data-input class="form-control input-numeric input-date text-center" data-target="#datetimepicker1"/>
+                            <input id="start-date" name="start-date" type="text" data-input class="form-control input-numeric input-date text-center" data-target="#datetimepicker1" value="<?=$_POST['start-date']?>"/>
                             <span class="input-group-text spancal" data-toggle>
                               <i class="fa fa-calendar"></i>
                             </span>
@@ -167,7 +212,7 @@
                         </div>
                         <div class="col-2">
                             <div class="input-group date" id="datetimepicker1" data-target-input="nearest">
-                              <input id="dt2" type="text" data-input class="form-control input-numeric input-date text-center" data-target="#datetimepicker1"/>
+                              <input id="end-date" name="end-date" type="text" data-input class="form-control input-numeric input-date text-center" data-target="#datetimepicker1" value="<?=$_POST['end-date']?>"/>
                               <span class="input-group-text spancal" data-toggle>
                                 <i class="fa fa-calendar"></i>
                               </span>
@@ -176,22 +221,26 @@
 
                         <div class="col-2">
                           <div class="input-group">
-                            <input type="text" class="form-control" placeholder="Agent's Name" aria-label="Text input with radio button">
+                            <input type="text" name="name-product" class="form-control" placeholder="Tên sản phẩm" aria-label="Text input with radio button" value="<?=$_POST['name-product']?>">
                           </div>
                         </div>
                         <div class="col-2">
                           <div class="input-group">
-                            <select class="form-select" id="inputGroupSelect02">
-                              <option selected>Status</option>
-                              <option value="1">HP nhận tiền</option>
-                              <option value="2">HP đã thanh toán</option>
+                            <select name="status-order" class="form-select" id="inputGroupSelect02">
+                            <option value="">Trạng Thái Đơn Hàng</option>
+                              <?php
+                              foreach(wc_get_order_statuses() as $key => $name)
+                              {
+                                echo '<option value="'.$key.'">'.$name.'</option>';
+                              }
+                              ?>
                             </select>
                           </div>
                         </div>
 
                         <div class="col-2 d-flex align-content-center justify-content-center">
                           <div class="d-flex align-content-center align-middle align-self-center w-75">
-                            <button type="button" class="btn btn-primary w-100 h-50">Search <i class="fa-solid fa-magnifying-glass"></i></button>
+                            <button type="submit" class="btn btn-primary w-100 h-50">Search <i class="fa-solid fa-magnifying-glass"></i></button>
                           </div>
                         </div>
                         <div class="col-2 d-flex align-content-center justify-content-center">
@@ -201,6 +250,7 @@
                         </div>
                     </div>
                 </div>
+              </form>
             </div>
             </div>
           
@@ -225,177 +275,18 @@
               </tbody>
             </table>
           </div>
-
-          <h3 class="mt-4 mb-4 "> Thông tin chi tiết đơn hàng khi click vào một đơn hàng</h3>
-
-          <div class="accordion accordion-flush" id="accordionFlushExample">
-            <div class="accordion-item">
-              <h2 class="accordion-header" id="flush-heading1">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse1" aria-expanded="false" aria-controls="flush-collapse1">
-                  Tên khách hàng
-                </button>
-              </h2>
-              <div id="flush-collapse1" class="accordion-collapse collapse" aria-labelledby="flush-heading1" data-bs-parent="#accordionFlushExample">
-                <div class="accordion-body">
-                  Chỗ này là tên của khách hàng
-                </div>
-              </div>
-            </div>
-
-            <div class="accordion-item">
-              <h2 class="accordion-header" id="flush-heading2">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse2" aria-expanded="false" aria-controls="flush-collapse2">
-                  Số điện thoại khách hàng
-                </button>
-              </h2>
-              <div id="flush-collapse2" class="accordion-collapse collapse" aria-labelledby="flush-heading2" data-bs-parent="#accordionFlushExample">
-                <div class="accordion-body">
-                  Refer lúc khách hàng đăng ký
-                </div>
-              </div>
-            </div>
-
-            <div class="accordion-item">
-              <h2 class="accordion-header" id="flush-heading3">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse3" aria-expanded="false" aria-controls="flush-collapse3">
-                  Tài khoản ngân hàng của khách hàng
-                </button>
-              </h2>
-              <div id="flush-collapse3" class="accordion-collapse collapse" aria-labelledby="flush-heading3" data-bs-parent="#accordionFlushExample">
-                <div class="accordion-body">
-                  refer lúc thanh toán
-                </div>
-              </div>
-            </div>
-
-            <div class="accordion-item">
-              <h2 class="accordion-header" id="flush-heading4">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse4" aria-expanded="false" aria-controls="flush-collapse4">
-                  Tên của Agent
-                </button>
-              </h2>
-              <div id="flush-collapse4" class="accordion-collapse collapse" aria-labelledby="flush-heading4" data-bs-parent="#accordionFlushExample">
-                <div class="accordion-body">
-                  Tên của cộng tác viên
-                </div>
-              </div>
-            </div>
-
-            <div class="accordion-item">
-              <h2 class="accordion-header" id="flush-heading5">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse5" aria-expanded="false" aria-controls="flush-collapse5">
-                  Tên của đại lý mà cộng tác viên thuộc vào
-                </button>
-              </h2>
-              <div id="flush-collapse5" class="accordion-collapse collapse" aria-labelledby="flush-heading5" data-bs-parent="#accordionFlushExample">
-                <div class="accordion-body">
-                  có thể có hoặc không có
-                </div>
-              </div>
-            </div>
-
-            <div class="accordion-item">
-              <h2 class="accordion-header"id="flush-heading6">
-                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse6" aria-expanded="false" aria-controls="flush-collapse6">
-                    Tên sản phẩm
-                  </button>
-              </h2>
-              <div id="flush-collapse6" class="accordion-collapse collapse" aria-labelledby="flush-heading6" data-bs-parent="#accordionFlushExample">
-                <div class="accordion-body">
-                  Refer name sản phẩm <br>
-                  + Phần note nếu có
-                </div>
-              </div>
-            </div>
-
-            <div class="accordion-item">
-              <h2 class="accordion-header" id="flush-heading7">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse7" aria-expanded="false" aria-controls="flush-collapse7">
-                  Date time mua hàng
-                </button>
-              </h2>
-              <div id="flush-collapse7" class="accordion-collapse collapse" aria-labelledby="flush-heading7" data-bs-parent="#accordionFlushExample">
-                <div class="accordion-body">
-                  Date time định dạng yyyy/mm/dd 
-                  time
-                </div>
-              </div>
-            </div>
-
-            <div class="accordion-item">
-              <h2 class="accordion-header" id="flush-heading8">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse8" aria-expanded="false" aria-controls="flush-collapse8">
-                  Total amount
-                </button>
-              </h2>
-              <div id="flush-collapse8" class="accordion-collapse collapse" aria-labelledby="flush-heading8" data-bs-parent="#accordionFlushExample">
-                <div class="accordion-body">
-                  Số tiền mà cộng tác viên đăng. <br>
-                  Cũng chính là số tiền mà khách hàng phải chuyển khoản
-                </div>
-              </div>
-            </div>
-
-            <div class="accordion-item">
-              <h2 class="accordion-header" id="flush-heading9">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse9" aria-expanded="false" aria-controls="flush-collapse9">
-                  Trả cho agent
-                </button>
-              </h2>
-              <div id="flush-collapse9" class="accordion-collapse collapse" aria-labelledby="flush-heading9" data-bs-parent="#accordionFlushExample">
-                <div class="accordion-body">
-                  Số tiền trả cho người đăng bài
-                </div>
-              </div>
-            </div>
-
-            <div class="accordion-item">
-              <h2 class="accordion-header" id="flush-heading10">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse10" aria-expanded="false" aria-controls="flush-collapse10">
-                  Số tiền trả lại cho host
-                </button>
-              </h2>
-              <div id="flush-collapse10" class="accordion-collapse collapse" aria-labelledby="flush-heading10" data-bs-parent="#accordionFlushExample">
-                <div class="accordion-body">
-                  Trả cho website happy house <br>
-                  1% của số tiền. 
-                </div>
-              </div>
-            </div>
-
-            <div class="accordion-item">
-              <h2 class="accordion-header" id="flush-heading11">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse11" aria-expanded="false" aria-controls="flush-collapse11">
-                  Trạng thái
-                </button>
-              </h2>
-              <div id="flush-collapse11" class="accordion-collapse collapse" aria-labelledby="flush-heading11" data-bs-parent="#accordionFlushExample">
-              <div class="accordion-body">
-                HP nhận tiền từ khách. Khi khách vừa chuyển khoản cho HP thì sẽ noti cho admin, <br>
-                noti cho agent trên hệ thống. 
-              </div>
-            </div>
-
-            <div class="accordion-item">
-              <h2 class="accordion-header" id="flush-heading12">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse12" aria-expanded="false" aria-controls="flush-collapse12">
-                  Action
-                </button>
-              </h2>
-              <div id="flush-collapse12" class="accordion-collapse collapse" aria-labelledby="flush-heading12" data-bs-parent="#accordionFlushExample">
-                <div class="accordion-body">
-                  Mục đích để có thể chuyển qua thanh toán, hoặc thanh toán xong rồi chuyển qua trạng thái done. 
-                </div>
-              </div>
-            </div>
-          </div>
-
-        
         </div>
-
       </div>
     </div>
+<div>
+  <p></p>
+  <p></p>
+  <p></p>
+  <p></p>
+  <p style="text-align: center;">Lưu ý: Nếu trường Action bỏ trống, nghĩa là host chưa thanh toán tiền cho Agent</p>
 
+
+</div>
         <footer class="text-center text-lg-start text-white mt-5 footer-bg">
           <!-- Grid container -->
           <div class="container p-4">
